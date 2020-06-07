577c656e7dcab8252a2201efb9d32ea4bfa9aa42
Ahmed Elghandour <elghandour.12@gmail.com>
2020-06-07 22:31:53 +0200

    sw / caching / indexdb

---
+++ 3-indexDb/public/src/js/feed.js
@@ -0,0 +1,138 @@
+var shareImageButton = document.querySelector('#share-image-button');
+var createPostArea = document.querySelector('#create-post');
+var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
+var sharedMomentsArea = document.querySelector('#shared-moments');
+
+function openCreatePostModal() {
+  createPostArea.style.display = 'block';
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+
+    deferredPrompt.userChoice.then((result) => {
+      console.log(result);
+      if (result.outcome === 'dismissed') {
+        console.log('User cancelled installation');
+      } else {
+        console.log('user added to home screen');
+      }
+    });
+
+    deferredPrompt = null;
+  }
+}
+
+function closeCreatePostModal() {
+  createPostArea.style.display = 'none';
+}
+
+shareImageButton.addEventListener('click', openCreatePostModal);
+
+closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
+
+/**
+ * this function is deactivated for now.
+ */
+async function onSaveBtnClick(event) {
+  console.log('clicked');
+
+  if (!'cache' in window) {
+    return;
+  }
+
+  const cache = await caches.open('user-request');
+  console.log(cache);
+
+  await cache.addAll([
+    'https://httpbin.org/get',
+    '/src/images/sf-boat.jpg'
+  ]);
+  // you can take action after cach successfull added.
+  event.target.textContent = 'saved'
+
+}
+
+function clearCard() {
+  while(sharedMomentsArea.hasChildNodes()) {
+    sharedMomentsArea.removeChild(sharedMomentsArea.lastChild)
+  }
+}
+function updateCards(data) {
+  data.forEach((el) => {
+    if(el) createCard(el);
+  })
+}
+
+function createCard(data) {
+  var cardWrapper = document.createElement('div');
+  cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
+  var cardTitle = document.createElement('div');
+  cardTitle.className = 'mdl-card__title';
+  cardTitle.style.backgroundImage = `url(${data.thumbnail})`;
+  cardTitle.style.backgroundSize = 'cover';
+  cardTitle.style.height = '180px';
+  cardWrapper.appendChild(cardTitle);
+  var cardTitleTextElement = document.createElement('h2');
+  cardTitleTextElement.className = 'mdl-card__title-text';
+  cardTitleTextElement.textContent = data.title;
+  cardTitle.appendChild(cardTitleTextElement);
+  var cardSupportingText = document.createElement('div');
+  cardSupportingText.className = 'mdl-card__supporting-text';
+  cardSupportingText.textContent = data.location;
+  cardSupportingText.style.textAlign = 'center';
+  // const saveButton = document.createElement('button');
+  // saveButton.addEventListener('click', onSaveBtnClick)
+  // saveButton.textContent = 'Save';
+  // cardWrapper.appendChild(saveButton);
+  cardWrapper.appendChild(cardSupportingText);
+  componentHandler.upgradeElement(cardWrapper);
+  sharedMomentsArea.appendChild(cardWrapper);
+}
+
+
+/**
+ * ======= [Strategy] cache then network  ==========
+ *
+ */
+
+const url = 'https://pwagram-ffb2e.firebaseio.com/posts.json';
+let parsedFromNetwork = false;
+
+if ('caches' in window) {
+  caches.match(url)
+    .then(response => { 
+      console.log(response);
+
+      if (response) {
+        return response.json();
+      }
+    }).then(data => {
+      console.log(data);
+      
+      if(!parsedFromNetwork && data) {
+        clearCard();
+        updateCards(data);
+      }
+    })
+}
+
+
+fetch(url)
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (data) {
+    console.log(data);
+
+    parsedFromNetwork = true;
+    clearCard();
+    updateCards(data);
+  }).catch((error) => {
+    console.log(error);
+  })
+
+
+  /** =========== Fetching a post request============= */
+
+  /**
+   * POST request can not be stored in the cache
+   */
---
+++ 3-indexDb/public/sw.js
@@ -0,0 +1,377 @@
+/**
+ * ============== note ================
+ *
+ * Service worker is listen and interact with events from pages or even from other web apis
+ * like [ push notification, background sync ]
+ *
+ * Main important events:
+ *
+ * - Fetch
+ * - Push Notification
+ * - Notification Interaction
+ * - Background Sync
+ * - Service Worker Lifecycle
+ *
+ */
+
+/**
+ * ============ Service Worker lifecycle ============
+ *
+ * - app register sw.js through app.js
+ * - installation:
+ *     emits install events. this events doesn't fire when page refresh or open new tab.
+ *     it fires only at the first time to register.
+ *     or the sw.js file has some changes.
+ *     it also used to prechace some assets.
+ *
+ * - activation:
+ *     emits active events. sw now controls all pages of scope.
+ *     it runs once installation finished. but not every time
+ *
+ * - idle:
+ *     once there's no current process it enter this mode.
+ * - terminated:
+ *     it go tot sleep mode once it go to idle mode that no current process.
+ *     it can be waked again once events are comming in. such as fetch event.
+ */
+
+/**
+ * ============== note ================
+ *
+ * Service worker scope is by default the folder that it set in there.
+ * best place it to but it in the root web folder to apply all html files
+ */
+
+/**
+ * ===============  CACHE API ===============
+ *  - Store key-value pairs, request and response.
+ *  - Data can be fetched from the stored cache instead of network.
+ * - it can be accessed from both sw and ordinary js files on the pages.
+ *
+ *
+ *  METHODS:
+ *  - Cache.match(request, options)
+ *      Returns a Promise that resolves to the response associated with the first matching request in the Cache object.
+ *
+ *  - Cache.matchAll(request, options)
+ *      eturns a Promise that resolves to an array of all matching requests in the Cache object.
+ *
+ *  - Cache.add(request)
+ *       Takes a URL, retrieves it and adds the resulting response object to the given cache. This is functionally equivalent to calling fetch(), then using put() to add the results to the cache.
+ *
+ *  - Cache.addAll(requests)
+ *      Takes an array of URLs, retrieves them, and adds the resulting response objects to the given cache.
+ *
+ *  - Cache.put(request, response)
+ *      Takes both a request and its response and adds it to the given cache.
+ *
+ *  - Cache.delete(request, options)
+ *      Finds the Cache entry whose key is the request, returning a Promise that resolves to true if a matching Cache entry is found and deleted. If no Cache entry is found, the promise resolves to false.
+ *
+ *  - Cache.keys(request, options)
+ *       Returns a Promise that resolves to an array of Cache keys.
+ */
+
+
+(function () {
+    importScripts('/src/js/idb.js');
+
+    const CACHE_STATIC_NAME = "static-v28";
+    const CACHE_DYNAMIC_NAME = "dynamic-v28";
+    const assetsToCache = [
+        "/",
+        "/index.html ",
+        "/offline.html ",
+        "/src/js/app.js ",
+        "/src/js/feed.js ",
+        "/src/js/idb.js ",
+        "/src/js/fetch-polyfill.js ",
+        "/src/js/promise-polyfill.js ",
+        "/src/js/material.min.js",
+        "/src/css/app.css",
+        "/src/css/feed.css",
+        "/src/images/main-image.jpg",
+        "https://fonts.googleapis.com/css?family=Roboto:400,700",
+        "https://fonts.googleapis.com/icon?family=Material+Icons",
+        "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css",
+    ];
+
+    const dbPromise = idb.open('feed-store', 1, (db) => {
+        if (!db.objectStoreNames.contains('posts')) {
+            db.createObjectStore('posts', { keyPath: 'id' })
+        }
+    });
+
+    self.addEventListener("install", (event) => {
+        /**
+         * event.waitUntil
+         *  - it prevent complete installation until the inside condition finishs.
+         *
+         *  - the reason to use is to prevent sw interactions
+         *    before cache is ready. for example trying to
+         *    fetch some assets from cache but cache is not ready for now.
+         */
+        event.waitUntil(
+            caches.open(CACHE_STATIC_NAME).then((cache) => {
+                /**
+                 * app shell assets:
+                 *  - it means the all nessecary files that appears on the current page
+                 */
+                console.log("[Service worker] Precaching App shell");
+                /** cache.add(request)
+                 *  - adds only one file to the cache
+                 *  - request can be request object or URL
+                 */
+
+                /**
+                 * cache.add('/') is added because the default route is /
+                 * so without it you can't serve default route without.
+                 * instead you can do that: localhost:8080/index.html
+                 */
+
+                // cache.add('/');
+                // cache.add('/index.html ');
+                // cache.add('/src/js/app.js ');
+
+                /** precaching */
+                cache.addAll(assetsToCache);
+            })
+        );
+    });
+
+    /**
+     * activate event will be fired when all tabs are closed then open new tab or
+     * manualy unregester old version on skipwaiting through application panel
+     * to prevent breaking the last web app experience.
+     */
+    self.addEventListener("activate", (event) => {
+        /**
+         * clean up old version of caches
+         */
+        event.waitUntil(
+            (async function () {
+                const keList = await caches.keys();
+                return Promise.all(
+                    keList.map((key) => {
+                        if (
+                            key !== CACHE_STATIC_NAME &&
+                            key !== CACHE_DYNAMIC_NAME
+                        )
+                            return caches.delete(key);
+                    })
+                );
+            })()
+        );
+
+        return self.clients.claim();
+    });
+
+
+
+
+
+    /**
+     * fetch event is fired when some assets are loading
+     * such as img, script, css, manual fetch req, etc.
+     *
+     *
+     *
+     * ========== [Strategy] cache with fallback network ===========
+     */
+
+    // self.addEventListener('fetch', (event) => {
+    //     /**
+    //      * respondWith => expects a promise
+    //      */
+    //     event.respondWith(async function () {
+    //         /**
+    //          * in case requests is cached
+    //          */
+    //         let response;
+    //         response = await caches.match(event.request);
+    //         if (response) {
+    //             return response;
+    //         }
+
+    //         /**
+    //          * In case request is not cached:
+    //          *  - fetch reuest from the network then update dynamic cache
+    //          */
+    //         try {
+    //             response = await fetch(event.request);
+    //             const cache = await caches.open(CACHE_DYNAMIC_NAME);
+
+    //             if (
+    //                 /* prevent chrome extenions */
+    //                 event.request.url.includes('http')
+    //             ) {
+
+    //                 /**
+    //                  * IMPORTANT: Clone the response. A response is a stream
+    //                  * and because we want the browser to consume the response
+    //                  * as well as the cache consuming the response, we need
+    //                  * to clone it so we have two streams.
+    //                  */
+
+    //                 cache.put(event.request, response.clone());
+    //             }
+    //             /**
+    //              * response should be returned to be fetched in the browser
+    //              */
+    //             return response;
+    //         } catch (error) {
+    //             /**
+    //                 * Providing a fallback offline page
+    //                 */
+
+    //             /**
+    //              *  To search through all caches
+    //              *  return caches.match('/offline.html');
+    //              */
+
+    //             // to search trough a specific cache name
+    //             const cache = await caches.open(CACHE_STATIC_NAME);
+    //             return cache.match('/offline.html');
+    //         }
+
+    //     }())
+    // })
+
+
+
+
+    /**
+     * ====== [Strategy] cache only ========
+     */
+
+    // self.addEventListener('fetch', (event) => {
+    //     event.respondWith(caches.match(event.request))
+    // })
+
+
+
+
+    /**
+     * ======= [Strategy] cache only ========
+     */
+
+    // self.addEventListener('fetch', (event) => {
+    //     event.respondWith(fetch(event.request))
+    // })
+
+
+
+
+
+    /**
+     * ======= [Strategy] network with cache fallback ==========
+     */
+
+    // self.addEventListener('fetch', (event) => {
+    //     event.respondWith(async function () {
+    //         let response;
+    //         try {
+    //             response = await fetch(event.request);
+    //             const cache = await caches.open(CACHE_DYNAMIC_NAME);
+
+    //             if (event.request.url.includes('http')) {
+    //                 cache.put(event.request, response.clone());
+    //             }
+
+    //         } catch (error) {
+    //             response = await caches.match(event.request);
+    //         }
+    //         return response;
+
+    //     }());
+    // })
+
+
+
+
+
+    /**
+     * ======= [Strategy] cache then network dynamic caching ==========
+     *
+     */
+
+    const isInArray = (string, array) => {
+        let cachePath;
+        if (string.indexOf(self.origin)) {
+            cachePath = string.substring(self.origin.length);
+        } else {
+
+            cachePath = string;
+        }
+
+        return array.indexOf(cachePath) > -1;
+    }
+
+    const trimCache = async (cacheName, maxItems) => {
+        const cache = await caches.open(cacheName);
+        const keys = await cache.keys();
+        if (keys.length > maxItems) {
+            await cache.delete(keys[0]);
+            trimCache(cacheName, maxItems);
+            console.log(keys.length);
+        }
+    }
+
+    self.addEventListener("fetch", (event) => {
+        event.respondWith(
+            (async function () {
+                let response;
+                const url = 'https://pwagram-ffb2e.firebaseio.com/posts.json';
+
+                if (event.request.url.indexOf(url) > -1) {
+
+                    try {
+                        response = await fetch(event.request);
+                        const data = await (response.clone().json());
+                        data.forEach(async element => {
+                            if(element) {
+                                const db = await dbPromise;
+                                const tx = db.transaction('posts', 'readwrite');
+                                const store = tx.objectStore('posts');
+                                store.put(element);
+                                tx.complete;
+                            }
+                        }); 
+
+                        return response;
+
+                    } catch (error) {
+
+                    }
+                } else if (isInArray(event.request.url, assetsToCache)) {
+
+                    return (await caches.open(CACHE_STATIC_NAME)).match(event.request.url)
+                } else {
+                    response = await caches.match(event.request);
+                    if (response) {
+                        return response;
+                    }
+
+                    try {
+                        response = await fetch(event.request);
+                        const cache = await caches.open(CACHE_DYNAMIC_NAME);
+
+                        if (event.request.url.includes("http")) {
+                            await cache.put(event.request, response.clone());
+                            trimCache(CACHE_DYNAMIC_NAME, 100);
+                        }
+
+                        return response;
+                    } catch (error) {
+                        if (event.request.headers.get('accept').includes('text/html')) {
+                            const cache = await caches.open(CACHE_STATIC_NAME);
+                            return cache.match("/offline.html");
+                        }
+                    }
+                }
+
+            })()
+        );
+    });
+})();
+
